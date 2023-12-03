ad797b09b7dc28ab99612e2ebfe95efe05197f66
nathvirginia <148851437+nathvirginia@users.noreply.github.com>
2023-12-03 04:09:37 -0600

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -48,24 +48,30 @@ updateTime();
 setInterval(updateTime, 1000);
 
 function updateCity(event) {
-  let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
-  }
-  let cityTime = moment().tz(cityTimeZone);
-  let cityName = event.target.options[event.target.selectedIndex].text;
   let citiesElement = document.querySelector("#cities");
 
-  citiesElement.innerHTML = ` <div class="city">
-          <div>
-            <h2>${cityName}</h2>
-            <div class="date">${cityTime.format("LL")}</div>
-          </div>
-          <div class="time">${cityTime.format(
-            "h:mm:ss"
-          )}<span class="small"> ${cityTime.format("A")} </span></div>
+  clearInterval(window.cityUpdateInterval);
+
+  window.cityUpdateInterval = setInterval(() => {
+    let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
+    }
+    let cityTime = moment().tz(cityTimeZone);
+    let cityName = event.target.options[event.target.selectedIndex].text;
+
+    citiesElement.innerHTML = ` 
+      <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("LL")}</div>
         </div>
-  `;
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )}<span class="small"> ${cityTime.format("A")} </span></div>
+      </div>
+    `;
+  }, 1000);
 }
 
 let selectCityElement = document.querySelector("#select-city");
